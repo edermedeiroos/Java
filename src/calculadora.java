98e147d979863aa459c925e01739b3d35b38ca9e
Eder Medeiros <ederlissemedeiros@gmail.com>
2026-08-10 21:38:01 -0300

    feat(src): calculadora

---
+++ src/calculadora.java
@@ -0,0 +1,45 @@
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public double somar(double a, double b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public double subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
+    }
+
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
+    }
+}
+
+void main() {
+    Calculadora calc = new Calculadora();
+    System.out.println(calc.somar(1, 3.5));
+}
