5d54f80d146b417bbacf6ea24e086bc89a4c0fea
Madeira <ederlissemedeiros@gmail.com>
2026-08-12 03:44:58 -0300

    refactor(Calculadora): split files

--- src/lista_01/Calculadora.java
+++ src/lista_01/Calculadora.java
@@ -39,10 +39,4 @@ public class Calculadora {
         }
         return a / b;
     }
-
-    public static void main(String[] args) {
-        Calculadora calc = new Calculadora();
-
-        System.out.println(calc.somar(1, 3.5));
-    }
 }
---
+++ src/lista_01/Exercicio01.java
@@ -0,0 +1,9 @@
+package lista_01;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        System.out.println(calc.somar(1, 3.5));
+    }
+}
